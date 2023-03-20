d42ddc00c48d2f9c1aaee4c9404fd7e35709127e
salzh <zhongxiang721@163.com>
2023-03-20 10:32:29 +0800

    add getregistration api

--- api/mod/Misc.pm
+++ api/mod/Misc.pm
@@ -281,7 +281,7 @@ sub _write() {
 	($body) = $response =~ /(\n\n.+)$/i;
 	$tmp = '';
 	$to_read = $content_length - (length $body);
-	warn "$body: $content_length: $to_read";
+	warn "$response: $body: $content_length: $to_read";
 	while (1) {
 		$n = sysread($sock, $tmp, $to_read);
 		if ($n >= $to_read) {
