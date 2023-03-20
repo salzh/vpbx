336842ca9d03099e38120807c9d40dd4fb9787f7
salzh <zhongxiang721@163.com>
2023-03-20 15:19:49 +0800

    add test script

---
+++ api/t/getcallbackstate.pl
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk5QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.GM6bMPBYkj8PQ2qIAQ22X6uT8mFBOzGrM0JLESyAGQQ" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=getcallbackstate&callbackuuid=null";
