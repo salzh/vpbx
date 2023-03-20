13ba72749b075a9bf713825589376720cef20eee
salzh <zhongxiang721@163.com>
2023-03-20 15:22:13 +0800

    add test script

--- api/t/getcallbackstate.pl
+++ api/t/getcallbackstate.pl
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/usr/bin/perl
 
-curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk5QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.GM6bMPBYkj8PQ2qIAQ22X6uT8mFBOzGrM0JLESyAGQQ" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=getcallbackstate&callbackuuid=null";
+$uuid = shift || 'null';
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk5QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.GM6bMPBYkj8PQ2qIAQ22X6uT8mFBOzGrM0JLESyAGQQ" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=getcallbackstate&callbackuuid=$uuid"`;
