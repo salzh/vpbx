102e97e7749e928625414cd4d0f03e41ecd87935
salzh <zhongxiang721@163.com>
2023-03-21 13:35:25 +0800

    u

--- api/t/getcallbackstate.pl
+++ api/t/getcallbackstate.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 
 $uuid = shift || 'null';
-$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk5QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.GM6bMPBYkj8PQ2qIAQ22X6uT8mFBOzGrM0JLESyAGQQ" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=getcallbackstate&callbackuuid=$uuid"`;
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk5QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.GM6bMPBYkj8PQ2qIAQ22X6uT8mFBOzGrM0JLESyAGQQ" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=getcallbackstate&uuid=$uuid"`;
 print $out, "\n";
