deeb1e024ace5b05c14cff87b54bac7b18e02069
salzh <zhongxiang721@163.com>
2023-03-28 08:13:36 +0800

    add test script for transfer action

--- api/t/transfer.pl
+++ api/t/transfer.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
 $uuid = shift || 'null';
-$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk4QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.HrBpW6BjkJqwrv0BLUGFzqQtj9NdS5J1SlCibWHa8V4" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=transfer&uuid=$uuid&dest=8448008008"`;
+$direction = shift || 'incoming';
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMTk4QDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.HrBpW6BjkJqwrv0BLUGFzqQtj9NdS5J1SlCibWHa8V4" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=transfer&direction=$direction&uuid=$uuid&dest=8448008008"`;
 print $out, "\n";
