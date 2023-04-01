99e654aacf25deb8ed9221f99fc279c8b522c6a4
salzh <zhongxiang721@163.com>
2023-04-01 16:44:43 +0800

    holdaction

---
+++ api/t/hold.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+$uuid = shift || 'null';
+$direction = shift || 'incoming';
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=hold&direction=$direction&uuid=$uuid&dest=8448008008"`;
+print $out, "\n";
