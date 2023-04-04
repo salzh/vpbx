6a166675a2b7f3ce5c571a67c3da439676621e79
salzh <zhongxiang721@163.com>
2023-04-04 08:51:10 +0800

    test sendvoicemaildrop

--- api/mod/C2C.pm
+++ api/mod/C2C.pm
@@ -447,7 +447,7 @@ sub sendvoicemaildrop {
 		}
 	} else {
 		$response{error} = 1;
-		$response{message} =   "'not found";
+		$response{message} =   "voicemaildrop file not found";
 		$response{actionid} = $form{actionid};
 		&print_json_response(%response);
 		return;
