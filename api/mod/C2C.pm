a824c075473dcc7c46064ef0610a6aa8957ad715
salzh <zhongxiang721@163.com>
2023-04-04 09:06:35 +0800

    test sendvoicemaildrop

--- api/mod/C2C.pm
+++ api/mod/C2C.pm
@@ -405,7 +405,7 @@ sub sendvoicemaildrop {
 		return;
 	}
 	%data = &database_select_as_hash("select voicemaildrop_uuid,voicemaildrop_uuid,voicemaildrop_name,voicemaildrop_path,domain_name,ext from v_voicemaildrop where voicemaildrop_uuid='$id'",
-									 "voicemaildrop_uuid,voicemaildrop_name,voicemaildrop_path domain_name,ext");
+									 "voicemaildrop_uuid,voicemaildrop_name,voicemaildrop_path,domain_name,ext");
 	
 
 	if ($data{$id}{voicemaildrop_uuid}) {
