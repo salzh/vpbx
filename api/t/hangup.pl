33e475268fd12e6bd1ad36fd65eff95d47ec0ffe
salzh <zhongxiang721@163.com>
2024-10-24 13:22:08 +0800

    u

--- api/t/hangup.pl
+++ api/t/hangup.pl
@@ -2,5 +2,5 @@
 
 $uuid = shift || 'null';
 $direction = shift || 'incoming';
-$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://222.velantro.net:8080/pbx/index.pl?mod=C2C&action=hangup&direction=$direction&uuid=$uuid&dest=8448008008"`;
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://155.mango9.com:8080/pbx/index.pl?mod=C2C&action=hangup&direction=$direction&uuid=$uuid&dest=8448008008"`;
 print $out, "\n";
