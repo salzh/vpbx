90e6c9a48fc94f7ae13eb555a5a2af08f2d25975
salzh <zhongxiang721@163.com>
2025-06-24 16:17:23 +0800

    u

---
+++ api/t/makepowercall.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+$uuid = shift || 'null';
+$direction = shift || 'incoming';
+$auth = 'eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0';
+$out = `curl -H "Authorization: Bearer $auth" "http://155.mango9.com:8080/pbx/index.pl?mod=CRM&action=makepowercall&src=100&callerid=7474779513&dests=8882115404`;
+print $out, "\n";
