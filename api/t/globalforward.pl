366190b6081e9b85c1850c5580df8746d7e21f90
salzh <zhongxiang721@163.com>
2025-12-19 18:51:40 +0800

    u

---
+++ api/t/globalforward.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+$uuid = shift || 'null';
+$direction = shift || 'incoming';
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://155.mango9.com:8080/pbx/index.pl?mod=Globalforward&action=setforward&forwardstr=2136410533;8882115404;true;true"`;
+print $out, "\n";
