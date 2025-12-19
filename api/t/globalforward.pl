478592b30f86a9a0e844e2c68a64f53117151408
salzh <zhongxiang721@163.com>
2025-12-19 19:00:38 +0800

    u

--- api/t/globalforward.pl
+++ api/t/globalforward.pl
@@ -2,5 +2,5 @@
 
 $uuid = shift || 'null';
 $direction = shift || 'incoming';
-$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://155.mango9.com:8080/pbx/index.pl?mod=C2C&action=setforward&forwardstr=2136410533;8882115404;true;true"`;
+$out = `curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.ewogICJzdWIiOiAiMzAxQDIyMi52ZWxhbnRyby5uZXQiLAogICJhdWQiOiAiMjIyLnZlbGFudHJvLm5ldCIsCiAgImV4cCI6IDE2OTQwMjIxMDAKfQo.Nt9ccxmvwhmth2R_sXMyP0g1rbaW-59y8JjIzUNHdt0" "http://155.mango9.com:8080/pbx/index.pl?mod=C2C&action=setforward&forwardstr=8334679595;8882115405;true;true"`;
 print $out, "\n";
